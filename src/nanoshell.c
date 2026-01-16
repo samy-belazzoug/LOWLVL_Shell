e56d9d1fde6d85a7b0933de1ba9479b03b27d3ea
samy-belazzoug <samy.belazzoug@laplateforme.io>
2026-01-16 11:21:58 +0100

    LAST COMMIT BEFORE EXAM

--- src/nanoshell.c
+++ src/nanoshell.c
@@ -86,33 +86,37 @@ int main() {
             
             switch (pid) {
                 case -1: // Child creation process got an error 
-            puts("fork failed!!");
-            exit(EXIT_FAILURE); //Equivalent of exit(1)
+                puts("fork failed!!");
+                exit(EXIT_FAILURE); //Equivalent of exit(1)
+            
             case 0: // Child is created. We are in child's process    
-                printf("Currently child process with PID : %d\n",getpid());
+                printf("Currently child process with PID : %d\n---EXECVE OUTPUT : \n\n",getpid());
                 execve(pathname,argv,__environ);
                 exit(EXIT_SUCCESS); //Equivalent of exit(0)
+
             default: // We are in parent's process
                 puts("Actually parent process");
+                
                 int status;
                 pid_t terminated_pid = wait(&status);
             
                 if (terminated_pid == -1) { //
                     puts("wait failed");
                     return 1;
+                }
                 
                 if (WIFEXITED(status)) { // Checks if the child terminated normally (e.g : with exit() or returning from main)
                     /*
                     status value from wait() is a bitwise structure that
                     varies by Unix like system, WIFEXITED is a macro that can safely interprets it.
                     This macro is important to avoid misunderstanding of status!*/
-                    printf("Child exited with status: %d\n", WEXITSTATUS(status));
+                    printf("\nChild exited with status: %d\n", WEXITSTATUS(status));
                 }
                 printf("Parent process back with PID : %d\n",getpid());
                 }
             }
         }
-    }
-        
-    exit(EXIT_SUCCESS);   
+    exit(EXIT_SUCCESS);  
 }
+        
+     
