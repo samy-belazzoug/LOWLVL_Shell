2402bb26b72c1b7be1f3598b4b10ba18dffbb2bb
samy-belazzoug <samy.belazzoug@laplateforme.io>
2026-01-16 01:13:53 +0100

    Builtin functions WORKING!

--- src/bashr.c
+++ src/bashr.c
@@ -2,10 +2,21 @@
 #include "header/my_parsing.h"
 
 int main() {   
+    
     while (1) {
+        
         // ACT 1, GET THE INPUTS
+        
+        char cwd[FILENAME_MAX];
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+
+        } 
+        else {
+            printf("pwd not worked, well, thats weird.\n");
+        }
+
         char cmd[N_TTY_BUF_SIZE]; //Reminder : 4096 size buffer
-        printf("Input : "); 
+        printf("%s > ",cwd); 
         fgets(cmd,sizeof(cmd),stdin); //Always leave /n at the end of the word, we need to remove it!
         
         // ACT 2, CLEAN THE INPUTS
@@ -36,25 +47,38 @@ int main() {
             }
         }
 
+        //pwd
+        else if (strcmp(argv[0],"pwd") == 0) {
+            printf("%s\n",cwd);
+        }
+
         // cd
         else if (strcmp(argv[0],"cd") == 0) { 
-            char *path;
+            char *path = argv[1];
             // cd ~ AND cd
-            if (strcmp(argv[1],"~") || argv[1] == NULL) {
+            if (argv[1] == NULL || strcmp(argv[1],"~") == 0) {
                 path = getenv("HOME");
-                if (chdir(path) != 0) {
-                    printf("Couldn't find path : %s\n",path);
-                }
             }
             // cd ..
             else if (strcmp(argv[1], "..") == 0) {
-                
+                path = "..";
             }
 
+            // cd pathxyz
+            else {
+                path = argv[1];
+            }
+
+            // We try to change directory
+            if (chdir(path) != 0) {
+                printf("Couldn't find path : %s\n",path);
+            }
         }
-    }
+
         
         // ACT 5, EXECUTABLES
+    }
+        
     exit(EXIT_SUCCESS);   
 }
         
